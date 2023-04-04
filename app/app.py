c7d66e4ba452859dd4f07090b2428c37cd061170
Sabrina Harvey <harveysabrina81@gmail.com>
2023-04-03 21:23:36 -0500

    Added all files

---
+++ app/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+# MySQL database configuration
+db_config = {
+    'user': 'capstone',
+    'password': 'Password876!@#',
+    'host': 'localhost',
+    'database': 'jobListings',
+}
+
+# Connect to the database
+db = mysql.connector.connect(**db_config)
+cursor = db.cursor()
+
+@app.route('/', methods=['GET'])
+def homepage():
+    return render_template('home.html')
+
+# Define a route to display the content from the database on the webpage
+@app.route('/joblisting.html', methods=['GET', 'POST'])
+def joblisting():
+    # Handle search form submission
+    if request.method == 'POST':
+        search_term = request.form['search']
+        query = f"SELECT jobTitle, employer, DatePosted, status FROM jobs WHERE jobTitle LIKE '%{search_term}%'"
+        cursor.execute(query)
+        data = cursor.fetchall()
+    else:
+        # Execute a SELECT statement to retrieve all data from the database
+        query = "SELECT jobTitle, employer, DatePosted, status FROM jobs"
+        cursor.execute(query)
+        data = cursor.fetchall()
+    
+    # Render the template and pass the data to the template
+    return render_template('joblisting.html', data=data)
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
